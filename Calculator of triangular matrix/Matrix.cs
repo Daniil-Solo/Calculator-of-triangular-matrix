73e9e1c1ddf1eb3080153c57296f54ed795e7096
Даниил Соловьев <daniil.solowyow1723@gmail.com>
2021-03-24 16:38:12 +0500

    Добавил класс Operations и указал метод Summ

---
+++ Calculator of triangular matrix/Matrix.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_of_triangular_matrix
+{
+    class Matrix
+    {
+        int n;
+        double V;
+        char type;  //----------------------------------------FIX ME
+        double[] packed_form;
+        DataTable table;
+    }
+}
---
+++ Calculator of triangular matrix/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_of_triangular_matrix
+{
+    class Operations
+    {
+        public static Matrix Summ(Matrix A, Matrix B)
+        {
+            // ------------------------------------------- FIX ME
+        }
+    }
+}
