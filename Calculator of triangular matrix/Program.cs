a56e0a7036a357d0c3a5c065fafdcff38c56f581
Даниил Соловьев <daniil.solowyow1723@gmail.com>
2021-03-24 16:33:25 +0500

    Добавил класс Matrix и указал поля

---
+++ Calculator of triangular matrix/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_of_triangular_matrix
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
