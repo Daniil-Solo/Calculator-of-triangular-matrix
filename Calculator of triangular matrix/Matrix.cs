702be990c391db010219ffd6b5e0dca670f1a72e
Даниил Соловьев <daniil.solowyow1723@gmail.com>
2021-03-24 16:49:08 +0500

    Добавил конструктор Matrix и указал New_m

--- Calculator of triangular matrix/Matrix.cs
+++ Calculator of triangular matrix/Matrix.cs
@@ -14,5 +14,19 @@ namespace Calculator_of_triangular_matrix
         char type;  //----------------------------------------FIX ME
         double[] packed_form;
         DataTable table;
+        
+        public Matrix()
+        {
+            this.n = 0;
+            this.V = 0;
+            this.type = '0'; //---------------------------------FIX ME
+            this.packed_form = null;
+            this.table = null;
+        }
+
+        public static Matrix New_m(int sp, char name_m)
+        {
+            //--------------------------------------------------FIX ME
+        }
     }
 }
