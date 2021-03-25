a7b66b3b109f835ab100813fc9a96abc30baecbc
Даниил Соловьев <daniil.solowyow1723@gmail.com>
2021-03-25 21:40:46 +0500

    Отредактировал Summ

--- Calculator of triangular matrix/Operations.cs
+++ Calculator of triangular matrix/Operations.cs
@@ -8,46 +8,43 @@ namespace Calculator_of_triangular_matrix
 {
     class Operations
     {
-        /*    public static int Ind(int i, int j, int type, int n)
-          {
-              int k=0;
-             switch (type)
+        
+        public static int Ind(int i, int j, Category type, int n)
         {
-            case 1:// правый верхний
-                k=n*i-i*(i-1)/2+j;
-                break;
-            case 2://левый верхний
-                 k=n*i-i*(i+1)/2+j;
-                break;
-           case 3://левый нижний
-                k=i*(i+3)/2+j-n+1;
-                break;
-           case 4://правый нижний
-                k=i*(i+1)/2+j;
-                break;
-                    return k;
-        }
-          }
-
-
-          public static Matrix Summ(Matrix A, Matrix B)
-          {
-
-            Matrix C = new Matrix
+            int k=0;
+            switch (type)
             {
-                v = A.v,
-                n = A.n,
-                Type = A.Type
-            };
-            for (int i=0;i<A.n;i++)
-             {
+                case Category.top_right:// правый верхний
+                    k = n * i - i * (i - 1) / 2 + j;
+                    break;
+                case Category.top_left://левый верхний
+                    k = n * i - i * (i + 1) / 2 + j;
+                    break;
+                case Category.bot_left://левый нижний
+                    k = i * (i + 3) / 2 + j - n + 1;
+                    break;
+                case Category.bot_right://правый нижний
+                    k = i * (i + 1) / 2 + j;
+                    break;
+            }
+            return k;
+        }
+
+
+        public static Matrix Summ(Matrix A, Matrix B)
+        {
+            int sizePackedForm = A.N * (A.N + 1) / 2;
+            Matrix C = new Matrix('_', A.N, A.V, A.Type, new double[sizePackedForm]);
+            
+            for (int i = 0; i < sizePackedForm; i++)
+            {
                 C.Packed_form[i]=A.Packed_form[i]+B.Packed_form[i];
-              }
+            }
            return C;
-          }
+        }
 
 
-          public static Matrix Subtraction(Matrix A, Matrix B)
+        public static Matrix Subtraction(Matrix A, Matrix B)
         {
 
             Matrix C = new Matrix
@@ -64,27 +61,28 @@ namespace Calculator_of_triangular_matrix
         }
 
         public static Matrix Multiply(Matrix A, Matrix B)
-          {
-          }
+        {
 
-          public static Matrix Reverse_A(Matrix A)
-          {
-          }
+        }
 
-          public static Matrix Reverse_B(Matrix B)
-          {
-          }
+        public static Matrix Reverse_A(Matrix A)
+        {
+        }
 
-          public static Matrix Replace_A_B(Matrix A, Matrix B)
-          {
-          }
+        public static Matrix Reverse_B(Matrix B)
+        {
+        }
 
-          public static Matrix Replace_A_C(Matrix A, Matrix B)
-          {
-          }
+        public static Matrix Replace_A_B(Matrix A, Matrix B)
+        {
+        }
 
-           public static Matrix Replace_B_C(Matrix A, Matrix B)
-          {
-          }*/
+        public static Matrix Replace_A_C(Matrix A, Matrix B)
+        {
+        }
+
+        public static Matrix Replace_B_C(Matrix A, Matrix B)
+        {
+        }
     }
 }
