2ca8b7ca72dc44ea92be988b368081995eac1e64
Sayorri <81217325+Sayorri@users.noreply.github.com>
2021-03-25 15:49:24 +0300

    Update Operations.cs

--- Calculator of triangular matrix/Operations.cs
+++ Calculator of triangular matrix/Operations.cs
@@ -8,18 +8,52 @@ namespace Calculator_of_triangular_matrix
 {
     class Operations
     {
-        /*public static Matrix Summ(Matrix A, Matrix B)
+        /*public static int Ind(int i, int j, int type, int n)
+          {
+              int k=0;
+             switch (type)
         {
-            // ------------------------------------------- FIX ME
+            case 1:// правый верхний
+                k=n*i-i*(i-1)/2+j;
+                break;
+            case 2://левый верхний
+                 k=n*i-i*(i+1)/2+j;
+                break;
+           case 3://левый нижний
+                k=i*(i+3)/2+j-n+1;
+                break;
+           case 4://правый нижний
+                k=i*(i+1)/2+j;
+                break;
+          return k;
         }
-        publick static matrix Subtraction(matrix A, matrix B)
-        {
-        }
+          }
 
-        publick static matrix Multiply(matrix A, matrix B)
-        {
-        }
 
+          public static Matrix Summ(Matrix A, Matrix B)
+          {
+              int n=A.GetLength(0);
+              int[,] C = new int[n, n];
+            for(int i=0;i<n;i++)
+             {
+                  for(int j=0;j<n;j++)
+               C[i]=A[i][j]+B[i][j];
+              }
+           return C;
+          }
+
+
+          publick static matrix Subtraction(matrix A, matrix B)
+          {
+            int n=A.GetLength(0);
+              int[,] C = new int[n, n];
+            for(int i=0;i<n;i++)
+             {
+                  for(int j=0;j<n;j++)
+               C[i]=A[i][j]-B[i][j];
+              }
+           return C;
+          }
         publick static matrix Reverse_A(matrix A)
         {
         }
