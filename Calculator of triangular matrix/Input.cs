65eb7561009b0fe505d3a135ec79cb401680de2d
Daniil-Solo <daniil.solowyow1723@gmail.com>
2021-03-26 15:49:14 +0500

    Изменил вид формы ввода случайной матрицы. Начал реализовывать проверку введенных данных в Input

--- Calculator of triangular matrix/Input.cs
+++ Calculator of triangular matrix/Input.cs
@@ -12,6 +12,7 @@ namespace Calculator_of_triangular_matrix
 {
     public partial class Input_keyb : Form
     {
+
         public Input_keyb()
         {
             InitializeComponent();
@@ -22,19 +23,42 @@ namespace Calculator_of_triangular_matrix
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void Input_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button_make_Click(object sender, EventArgs e)
+        {
+            int n;
+            double V;
+            Category type;
+            bool success;
+
+            // проверка на условие принадлежности категории
+
+
+            success = Int32.TryParse(textBox_n.Text, out n);
+            if(!success)
+            {
+                // окошко с ошибкой 
+                textBox_n.Text = "";
+                return;
+            }
+            success = Double.TryParse(textBox_V.Text, out V);
+            if(!success)
+            {
+                // окно с ошибкой
+                textBox_V.Text = "";
+                return;
+            }
+
+            DataTransfer.data[0] = n;
+            DataTransfer.data[1] = V;
+            DataTransfer.data[2] = type;
+
+            // Открытие формы Input_hand
         }
     }
 }
